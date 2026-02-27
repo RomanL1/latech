b57c383ba0ac075a6c083b256dcca428b74301ee
Roman Lysser <lysserroman@gmail.com>
2026-02-27 16:42:04 +0100

    feature(api): initialize project

---
+++ src/api/src/main/java/com/latech/api/LatechApiApplication.java
@@ -0,0 +1,15 @@
+package com.latech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatechApiApplication
+{
+
+	public static void main ( String[] args )
+	{
+		SpringApplication.run( LatechApiApplication.class, args );
+	}
+
+}
---
+++ src/api/src/test/java/com/latech/api/LatechApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.latech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatechApiApplicationTests
+{
+
+	@Test
+	void contextLoads ()
+	{
+	}
+
+}
