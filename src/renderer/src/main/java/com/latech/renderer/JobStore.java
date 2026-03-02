a031a996468784c7635550e818536500258076c9
Marc Muhmenthaler <bull3t@bluewin.ch>
2026-03-02 12:30:11 +0100

    refactor (JobStore, LatexToPdfService): JobStore.get() returns Optional
    JobStore.get() now returns an Optional<Entry>.
    Refactored LatexToPdfService's methods to reflect that change.
    JobStore now also stores "message" as an empty String instead of null for Status: QUEUED, RUNNING and DONE.
    pdf can still be null, but will be revised once storage is clarified.
    #12

--- src/renderer/src/main/java/com/latech/renderer/JobStore.java
+++ src/renderer/src/main/java/com/latech/renderer/JobStore.java
@@ -3,17 +3,18 @@ package com.latech.renderer;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class JobStore {
     private final Map<String, Entry> store = new ConcurrentHashMap<>();
 
-    public void queued(String id) { store.put(id, new Entry(State.QUEUED, null, null)); }
-    public void running(String id) { store.put(id, new Entry(State.RUNNING, null, null)); }
+    public void queued(String id) { store.put(id, new Entry(State.QUEUED, "", null)); }
+    public void running(String id) { store.put(id, new Entry(State.RUNNING, "", null)); }
     //TODO(marc): probably shouldn't store entire pdf, instead store only a path to the file.
-    public void done(String id, byte[] pdf) { store.put(id, new Entry(State.DONE, null, pdf)); }
+    public void done(String id, byte[] pdf) { store.put(id, new Entry(State.DONE, "", pdf)); }
     public void failed(String id, String msg) { store.put(id, new Entry(State.FAILED, msg, null)); }
-    public Entry get(String id) { return store.get(id); }
+    public Optional<Entry> get(String id) { return Optional.ofNullable(store.get(id)); }
     public void deleteEntry(String id) { store.remove(id); }
 }
--- src/renderer/src/main/java/com/latech/renderer/api/LatexToPdfService.java
+++ src/renderer/src/main/java/com/latech/renderer/api/LatexToPdfService.java
@@ -1,10 +1,7 @@
 package com.latech.renderer.api;
 
 import com.latech.pdf.v1.*;
-import com.latech.renderer.JobStore;
-import com.latech.renderer.PdfJobManager;
-import com.latech.renderer.QueueFullException;
-import com.latech.renderer.State;
+import com.latech.renderer.*;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
@@ -12,11 +9,11 @@ import org.springframework.grpc.server.service.GrpcService;
 @GrpcService
 public class LatexToPdfService extends PdfServiceGrpc.PdfServiceImplBase {
     private final PdfJobManager manager;
-    private final JobStore store;
+    private final JobStore jobStore;
 
-    public LatexToPdfService(PdfJobManager manager, JobStore store) {
+    public LatexToPdfService(PdfJobManager manager, JobStore jobStore) {
         this.manager = manager;
-        this.store = store;
+        this.jobStore = jobStore;
     }
 
     @Override
@@ -39,62 +36,68 @@ public class LatexToPdfService extends PdfServiceGrpc.PdfServiceImplBase {
 
     @Override
     public void getJobStatus(GetJobStatusRequest req, StreamObserver<GetJobStatusResponse> obs) {
-        var e = store.get(req.getJobId());
-        if (e == null) {
-            obs.onError(Status.NOT_FOUND.withDescription("Unknown jobId").asRuntimeException());
-            return;
-        }
 
-        obs.onNext(GetJobStatusResponse.newBuilder()
-                .setJobId(req.getJobId())
-                .setState(toProtoState(e.state().name()))
-                .setMessage(e.message() == null ? "" : e.message())
-                .build());
-        obs.onCompleted();
+        jobStore.get(req.getJobId())
+                .ifPresentOrElse(
+                        job -> {
+                            obs.onNext(GetJobStatusResponse.newBuilder()
+                                    .setJobId(req.getJobId())
+                                    .setState(toProtoState(job.state().name()))
+                                    .setMessage(job.message())
+                                    .build());
+                            obs.onCompleted();
+                        },
+                        () -> obs.onError(Status.NOT_FOUND
+                                .withDescription("Unknown jobId")
+                                .asRuntimeException())
+                );
     }
 
     @Override
     public void getPdf(GetPdfRequest req, StreamObserver<GetPdfChunk> obs) {
-        var e = store.get(req.getJobId());
 
-        if (e == null) {
+        var jobOptional = jobStore.get(req.getJobId());
+        if (jobOptional.isEmpty()){
             obs.onError(Status.NOT_FOUND
                     .withDescription("unknown JobId")
                     .asRuntimeException());
             return;
         }
 
-        if (e.state() == State.RUNNING || e.state() == State.QUEUED) {
-            obs.onError(Status.FAILED_PRECONDITION
-                    .withDescription("Not ready")
-                    .asRuntimeException());
-            return;
+        Entry job = jobOptional.get();
+
+        switch(job.state()){
+            case State.RUNNING, State.QUEUED -> {
+                obs.onError(Status.FAILED_PRECONDITION
+                        .withDescription("Not ready")
+                        .asRuntimeException());
+            }
+            case State.FAILED -> {
+                obs.onError(Status.INTERNAL
+                        .withDescription("Failed: " + job.message())
+                        .asRuntimeException());
+
+                //TODO(marc): do we delete the entry here? API needs to handle failed jobs correctly.
+                jobStore.deleteEntry(req.getJobId());
+            }
+
+            case State.DONE -> {
+                //TODO(marc): maybe don't load entire pdf, instead stream it from disc aswell.
+                byte[] pdf = job.pdfBytes();
+                int chunkSize = 64 * 1024;
+                for (int i = 0; i < pdf.length; i += chunkSize) {
+                    int len = Math.min(chunkSize, pdf.length - i);
+                    obs.onNext(GetPdfChunk.newBuilder()
+                            .setData(com.google.protobuf.ByteString.copyFrom(pdf, i, len))
+                            .build());
+                }
+                obs.onCompleted();
+
+                //TODO(marc): delete pdf from disc if we store only the path in store.
+                // assuming API handles getting the file once and redirects following requests away from renderer.
+                jobStore.deleteEntry(req.getJobId());
+            }
         }
-
-        if (e.state() == State.FAILED) {
-            obs.onError(Status.INTERNAL
-                    .withDescription("Failed: " + e.message())
-                    .asRuntimeException());
-
-            //TODO(marc): do we delete the entry here? API needs to handle failed jobs correctly.
-            store.deleteEntry(req.getJobId());
-            return;
-        }
-
-        //TODO(marc): maybe don't load entire pdf, instead stream it from disc aswell.
-        byte[] pdf = e.pdfBytes();
-        int chunkSize = 64 * 1024;
-        for (int i = 0; i < pdf.length; i += chunkSize) {
-            int len = Math.min(chunkSize, pdf.length - i);
-            obs.onNext(GetPdfChunk.newBuilder()
-                    .setData(com.google.protobuf.ByteString.copyFrom(pdf, i, len))
-                    .build());
-        }
-        obs.onCompleted();
-
-        //TODO(marc): delete pdf from disc if we store only the path in store.
-        // assuming API handles getting the file once and redirects following requests away from renderer.
-        store.deleteEntry(req.getJobId());
     }
 
     private static JobState toProtoState(String s) {
