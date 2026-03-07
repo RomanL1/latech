eccf963514ba20e223121532cce0927a39bd2674
Roman Lysser <lysserroman@gmail.com>
2026-03-07 16:24:27 +0100

    refactor(api): add DLQ and DLX configuration for pdf_rendered queue

--- src/api/src/main/java/com/latech/api/config/RabbitMQConfig.java
+++ src/api/src/main/java/com/latech/api/config/RabbitMQConfig.java
@@ -14,12 +14,14 @@ public class RabbitMQConfig
 	public static final String LATECH_TOPIC = "latech.topic";
 
 	public static final String DOCUMENT_EXCHANGE = "document_exchange";
-	public static final String PDF_RENDERED = "pdf_rendered";
 	public static final String ROUTING_KEY_DOCUMENT_EXCHANGE = DOCUMENT_EXCHANGE + ".#";
-	public static final String ROUTING_KEY_PDF_RENDERED = PDF_RENDERED + ".#";
-
 	public static final String DOCUMENT_EXCHANGE_DLQ = DOCUMENT_EXCHANGE + ".dlq";
 	public static final String DOCUMENT_EXCHANGE_DLX = DOCUMENT_EXCHANGE + ".dlx";
+
+	public static final String PDF_RENDERED = "pdf_rendered";
+	public static final String ROUTING_KEY_PDF_RENDERED = PDF_RENDERED + ".#";
+	public static final String PDF_RENDERED_DLQ = PDF_RENDERED + ".dlq";
+	public static final String PDF_RENDERED_DLX = PDF_RENDERED + ".dlx";
 
 	@Bean
 	public TopicExchange topic ()
@@ -56,20 +58,43 @@ public class RabbitMQConfig
 	}
 
 	@Bean
+	public Binding bindingDocumentExchangeQueue ()
+	{
+		return BindingBuilder.bind( initiateDocumentExchangeQueue() ).to( topic() )
+				.with( ROUTING_KEY_DOCUMENT_EXCHANGE );
+	}
+
+	@Bean
+	public TopicExchange pdfRenderedDlx ()
+	{
+		return new TopicExchange( PDF_RENDERED_DLX );
+	}
+
+	@Bean
+	public Queue pdfRenderedDlq ()
+	{
+		return new Queue( PDF_RENDERED_DLQ, true );
+	}
+
+	@Bean
+	public Binding bindingPDFRenderedDlq ( Queue pdfRenderedDlq, TopicExchange pdfRenderedDlx )
+	{
+		return BindingBuilder.bind( pdfRenderedDlq ).to( pdfRenderedDlx ).with( ROUTING_KEY_PDF_RENDERED );
+	}
+
+	@Bean
 	public Queue initiatePDFRenderedQueue ()
 	{
-		return new Queue( PDF_RENDERED, true );
+		return org.springframework.amqp.core.QueueBuilder.durable( PDF_RENDERED )
+				.withArgument( "x-dead-letter-exchange", PDF_RENDERED_DLX )
+				.withArgument( "x-dead-letter-routing-key", ROUTING_KEY_PDF_RENDERED )
+				.build();
 	}
 
 	@Bean
-	public Binding bindingDocumentExchangeQueue ( Queue initiateDocumentExchangeQueue, TopicExchange topic )
+	public Binding bindingPDFRenderedQueue ()
 	{
-		return BindingBuilder.bind( initiateDocumentExchangeQueue ).to( topic ).with( ROUTING_KEY_DOCUMENT_EXCHANGE );
-	}
-
-	@Bean
-	public Binding bindingPDFRenderedQueue ( Queue initiatePDFRenderedQueue, TopicExchange topic )
-	{
-		return BindingBuilder.bind( initiatePDFRenderedQueue ).to( topic ).with( ROUTING_KEY_PDF_RENDERED );
+		return BindingBuilder.bind( initiatePDFRenderedQueue() ).to( topic() )
+				.with( ROUTING_KEY_PDF_RENDERED );
 	}
 }
