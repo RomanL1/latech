c435d8ef3e5a6131fff0ba9a12ad7f8f68a0e7ac
Andri Wandres <35061384+andriwandres@users.noreply.github.com>
2026-03-14 13:10:31 +0100

    feat(frontend): initialize radix-ui/themes (#45)

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Theme } from '@radix-ui/themes';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
@@ -10,15 +11,17 @@ import LatexEditor from './shared/components/LatexEditor';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PageLayout />}>
-          <Route index element={<LandingPage />} />
-          <Route path="create" element={<CreateDocumentPage />} />
-          <Route path="document/:documentId" element={<DocumentPage />} />
-          <Route path="editor" element={<LatexEditor />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Theme>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageLayout />}>
+            <Route index element={<LandingPage />} />
+            <Route path="create" element={<CreateDocumentPage />} />
+            <Route path="document/:documentId" element={<DocumentPage />} />
+            <Route path="editor" element={<LatexEditor />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Theme>
   </StrictMode>,
 );
