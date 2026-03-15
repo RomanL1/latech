2b7591f7eb14be95b1b297209380b838af748f35
Roman Lysser <lysserroman@gmail.com>
2026-03-15 17:46:56 +0100

    refactor(backend): move secure document to DocumentController

--- src/api/src/main/java/com/latech/api/api/DocumentController.java
+++ src/api/src/main/java/com/latech/api/api/DocumentController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.latech.api.model.DocumentCreateRequestDto;
 import com.latech.api.model.DocumentCreateResponseDto;
 import com.latech.api.model.DocumentDto;
+import com.latech.api.model.DocumentSecuredCreateRequestDto;
+import com.latech.api.model.DocumentSecuredRequestDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -66,6 +68,43 @@ public class DocumentController
 		//todo initiate render via rabbitmq
 
 		return ResponseEntity.accepted().build();
+	}
+
+	@PostMapping( "secured" )
+	public ResponseEntity<DocumentCreateResponseDto> createDocumentSecured (
+			@RequestBody DocumentSecuredCreateRequestDto documentSecuredCreateRequestDto )
+	{
+		if ( ObjectUtils.isEmpty( documentSecuredCreateRequestDto ) || ObjectUtils.isEmpty(
+				documentSecuredCreateRequestDto.getName() ) || ObjectUtils.isEmpty(
+				documentSecuredCreateRequestDto.getPassword() ) )
+		{
+			return ResponseEntity.badRequest().build();
+		}
+		// Todo secured document creation logic
+		var response = DocumentCreateResponseDto.builder()
+				.uuid( UUID.randomUUID().toString() )
+				.name( documentSecuredCreateRequestDto.getName() )
+				.build();
+		return ResponseEntity.ok( response );
+	}
+
+	@PostMapping( "secured/{docId}" )
+	public ResponseEntity<DocumentDto> getDocumentSecuredContent ( @PathVariable String docId,
+			@RequestBody DocumentSecuredRequestDto documentSecuredRequestDto )
+	{
+		if ( ObjectUtils.isEmpty( docId ) || ObjectUtils.isEmpty( documentSecuredRequestDto ) || ObjectUtils.isEmpty(
+				documentSecuredRequestDto.getPassword() ) )
+		{
+			return ResponseEntity.badRequest().build();
+		}
+		//todo check password
+		//todo load document
+		var response = DocumentDto.builder()
+				.id( docId )
+				.name( "documentname" )
+				.content( "in future your content will be loaded from backend" )
+				.build();
+		return ResponseEntity.ok( response );
 	}
 
 }
