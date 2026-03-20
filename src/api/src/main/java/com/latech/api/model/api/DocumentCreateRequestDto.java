95527d21df4906385e2bb1427c2f29cd535cc514
Roman Lysser <lysserroman@gmail.com>
2026-03-20 17:09:18 +0100

    fix(backend): jackson error

--- src/api/src/main/java/com/latech/api/model/api/DocumentCreateRequestDto.java
+++ src/api/src/main/java/com/latech/api/model/api/DocumentCreateRequestDto.java
@@ -1,11 +1,15 @@
 package com.latech.api.model.api;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DocumentCreateRequestDto
 {
 	private String name;
--- src/api/src/main/java/com/latech/api/model/api/DocumentSecuredRequestDto.java
+++ src/api/src/main/java/com/latech/api/model/api/DocumentSecuredRequestDto.java
@@ -1,10 +1,17 @@
 package com.latech.api.model.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DocumentSecuredRequestDto
 {
 	private String password;
