986d4c3a8c93a296e3ba883cd37a81c379a2a4fa
Gregor Lazzarato <gregor.lazzarato@stud.hslu.ch>
2026-03-26 17:35:13 +0100

    feat: add websocket server functionylit + create room with id from url params

---
+++ src/socket-server/src/callback.js
@@ -0,0 +1,82 @@
+import http from 'http'
+import * as number from 'lib0/number'
+
+const CALLBACK_URL = process.env.CALLBACK_URL ? new URL(process.env.CALLBACK_URL) : null
+const CALLBACK_TIMEOUT = number.parseInt(process.env.CALLBACK_TIMEOUT || '5000')
+const CALLBACK_OBJECT_NAME = process.env.CALLBACK_OBJECT_NAME
+// const CALLBACK_OBJECTS = process.env.CALLBACK_OBJECTS ? JSON.parse(process.env.CALLBACK_OBJECTS) : {}
+
+export const isCallbackSet = !!CALLBACK_URL
+
+/**
+ * @param {import('./utils.js').WSSharedDoc} doc
+ */
+export const callbackHandler = (doc) => {
+  const room = doc.name.split('/')[1] // Assuming room name is the first part of the doc name before a hyphen
+  const dataToSend = {
+    room,
+    data: doc.getText(CALLBACK_OBJECT_NAME).toJSON()
+  }
+
+  console.log("data:", dataToSend)
+
+  // For simplicity, we only send the content of a single Y.Text object in the callback request.
+  // const sharedObjectList = Object.keys(CALLBACK_OBJECTS)
+  // sharedObjectList.forEach(sharedObjectName => {
+  //   const sharedObjectType = CALLBACK_OBJECTS[sharedObjectName]
+  //   dataToSend.data[sharedObjectName] = {
+  //     type: sharedObjectType,
+  //     content: getContent(sharedObjectName, sharedObjectType, doc).toJSON()
+  //   }
+  // })
+  CALLBACK_URL && callbackRequest(CALLBACK_URL, CALLBACK_TIMEOUT, dataToSend)
+}
+
+/**
+ * @param {URL} url
+ * @param {number} timeout
+ * @param {Object} data
+ */
+const callbackRequest = (url, timeout, data) => {
+  console.log("Sending callback request to:", url.href)
+
+  data = JSON.stringify(data)
+  const options = {
+    hostname: url.hostname,
+    port: url.port,
+    path: url.pathname,
+    timeout,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  }
+  const req = http.request(options)
+  req.on('timeout', () => {
+    console.warn('Callback request timed out.')
+    req.abort()
+  })
+  req.on('error', (e) => {
+    console.error('Callback request error.', e)
+    req.abort()
+  })
+  req.write(data)
+  req.end()
+}
+
+/**
+ * @param {string} objName
+ * @param {string} objType
+ * @param {import('./utils.js').WSSharedDoc} doc
+ */
+const getContent = (objName, objType, doc) => {
+  switch (objType) {
+    case 'Array': return doc.getArray(objName)
+    case 'Map': return doc.getMap(objName)
+    case 'Text': return doc.getText(objName)
+    case 'XmlFragment': return doc.getXmlFragment(objName)
+    case 'XmlElement': return doc.getXmlElement(objName)
+    default : return {}
+  }
+}
