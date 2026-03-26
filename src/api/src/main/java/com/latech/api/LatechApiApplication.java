86808f756ac8b358a7246f44e5dee7768834fea2
Roman Lysser <lysserroman@gmail.com>
2026-03-26 14:50:31 +0100

    refactor(api): cleanup dependencies

--- src/api/src/main/java/com/latech/api/LatechApiApplication.java
+++ src/api/src/main/java/com/latech/api/LatechApiApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.grpc.client.ImportGrpcClients;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -18,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
-@ImportGrpcClients( basePackages = "greet" )
 public class LatechApiApplication implements ApplicationListener<ContextRefreshedEvent>
 {
 
