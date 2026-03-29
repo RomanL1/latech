2ec8a9a4804d92b4d40c7a95204a5ecd95b506ff
Roman Lysser <lysserroman@gmail.com>
2026-03-29 20:43:38 +0200

    ci(api): fix failing test

--- src/api/src/test/java/com/latech/api/LatechApiApplicationTests.java
+++ src/api/src/test/java/com/latech/api/LatechApiApplicationTests.java
@@ -2,10 +2,17 @@ package com.latech.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LatechApiApplicationTests
 {
+
+	@MockitoBean
+	private ConnectionFactory connectionFactory;
 
 	@Test
 	void contextLoads ()
