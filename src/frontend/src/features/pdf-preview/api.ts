ea67c46643fbf9e26d9b8e6b8b07fbcc7bd4858b
Roman Lysser <lysserroman@gmail.com>
2026-04-11 20:12:38 +0200

    feat: add pdf preview (WIP)

---
+++ src/frontend/src/features/pdf-preview/api.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+
+const apiHost = window.ENV.VITE_API_HOST;
+const documentUrl = `${apiHost}/document/`;
+
+export function requestPDFRender(docId: string): Promise<void> {
+  return fetch(`${documentUrl}${docId}/render`, { method: 'POST' })
+    .then((response) => {
+      if (!response.ok) throw new Error('Failed to initiate render');
+    })
+    .catch(() => Promise.resolve<void>(undefined));
+}
+
+export function getRenderedPDF(docId: string): Promise<Blob> {
+  return fetch(`${documentUrl}${docId}/render`).then((res) => res.blob());
+}
+
+export function usePDFRenderQuery() {
+  return useQuery({
+    queryKey: ['pdfrender'],
+    queryFn: requestPDFRender,
+  });
+}
+
+export function getPDFRenderedEventSource(docId: string): EventSource {
+  return new EventSource(`${documentUrl}${docId}/stream-updates`);
+}
+
+
