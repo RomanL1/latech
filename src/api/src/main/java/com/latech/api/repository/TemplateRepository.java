a73cecb592c871737138ae2fecc2226787ddad84
thommm4999 <thomassartini@hotmail.com>
2026-05-01 14:07:13 +0200

    feat(api): add seeded latex templates and renderer package support (#85)
    * feat(api): add seeded latex templates and renderer package support
    
    * fix: Dockerfile updated (imports)

--- src/api/src/main/java/com/latech/api/repository/TemplateRepository.java
+++ src/api/src/main/java/com/latech/api/repository/TemplateRepository.java
@@ -3,7 +3,9 @@ package com.latech.api.repository;
 import com.latech.api.model.db.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TemplateRepository extends JpaRepository<Template, UUID> {
+    Optional<Template> findByName ( String name );
 }
