d7120c6ce67177e5ef86957c5fa8006eb6d65cba
Andri Wandres <35061384+andriwandres@users.noreply.github.com>
2026-05-22 14:31:58 +0200

    refactor(frontend): wrap Yjs editor setup in provider (#114)
    * refactor(frontend): wrap Yjs editor setup in provider
    
    * fix: cursor position on windows
    
    * line endings?
    
    * revert

---
+++ src/frontend/src/shared/components/latex-editor/EditorContext.ts
@@ -0,0 +1,37 @@
+import { editor as MonacoEditor } from 'monaco-editor';
+import { createContext, type Dispatch, type SetStateAction, useContext } from 'react';
+import { WebsocketProvider } from 'y-websocket';
+import * as Y from 'yjs';
+
+export type AwarenessUser = {
+  clientId: number;
+  user?: {
+    name?: string;
+    color: string;
+  };
+};
+
+export type AwarenessUserList = Map<number, AwarenessUser>;
+
+export interface EditorContextValue {
+  awarenessUsers: AwarenessUserList;
+  currentAwarenessUser: AwarenessUser | null;
+  editor: MonacoEditor.IStandaloneCodeEditor | null;
+  isConnected: boolean;
+  setEditor: Dispatch<SetStateAction<MonacoEditor.IStandaloneCodeEditor | null>>;
+  yDoc: Y.Doc | null;
+  yProvider: WebsocketProvider | null;
+  yText: Y.Text | null;
+}
+
+export const EditorContext = createContext<EditorContextValue | null>(null);
+
+export function useEditor() {
+  const value = useContext(EditorContext);
+
+  if (!value) {
+    throw new Error('useEditor must be used within an EditorProvider');
+  }
+
+  return value;
+}
