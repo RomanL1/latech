1c86a532a4e364c4137802d054618df86c0a0ab6
Andri Wandres <andrilucawandres@gmail.com>
2026-05-24 00:16:18 +0200

    feat(frontend): support toggle for latex macro controls

--- src/frontend/src/pages/document/editor-view/controls/font-size/FontSizeControl.tsx
+++ src/frontend/src/pages/document/editor-view/controls/font-size/FontSizeControl.tsx
@@ -1,36 +1,36 @@
 import { Text } from '@radix-ui/themes';
-import { useEditor } from '../../../../../shared/components/latex-editor/EditorContext';
+import { LatexMacro, useEditor } from '../../../../../shared/components/latex-editor/EditorContext';
 import { NonFocusStealingDropdown } from '../../../../../shared/components/non-focus-stealing-dropdown/NonFocusStealingDropdown';
 
 interface FontSizeMapping {
-  latexMacro: string;
+  latexMacroName: string;
   friendlyName: string;
   fontSize: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
 }
 
 const fontSizes: FontSizeMapping[] = [
-  { latexMacro: '\\Huge', friendlyName: 'Giant', fontSize: '9' },
-  { latexMacro: '\\huge', friendlyName: 'Huge', fontSize: '8' },
-  { latexMacro: '\\LARGE', friendlyName: 'Larger', fontSize: '7' },
-  { latexMacro: '\\Large', friendlyName: 'Bold', fontSize: '6' },
-  { latexMacro: '\\large', friendlyName: 'Emphasis', fontSize: '5' },
-  { latexMacro: '\\small', friendlyName: 'Compact', fontSize: '4' },
-  { latexMacro: '\\footnotesize', friendlyName: 'Caption', fontSize: '3' },
-  { latexMacro: '\\scriptsize', friendlyName: 'Tiny', fontSize: '2' },
-  { latexMacro: '\\tiny', friendlyName: 'Micro', fontSize: '1' },
+  { latexMacroName: 'Huge', friendlyName: 'Giant', fontSize: '9' },
+  { latexMacroName: 'huge', friendlyName: 'Huge', fontSize: '8' },
+  { latexMacroName: 'LARGE', friendlyName: 'Larger', fontSize: '7' },
+  { latexMacroName: 'Large', friendlyName: 'Bold', fontSize: '6' },
+  { latexMacroName: 'large', friendlyName: 'Emphasis', fontSize: '5' },
+  { latexMacroName: 'small', friendlyName: 'Compact', fontSize: '4' },
+  { latexMacroName: 'footnotesize', friendlyName: 'Caption', fontSize: '3' },
+  { latexMacroName: 'scriptsize', friendlyName: 'Tiny', fontSize: '2' },
+  { latexMacroName: 'tiny', friendlyName: 'Micro', fontSize: '1' },
 ];
 
 const FontSizeControl = () => {
-  const { surroundSelectionOrWord } = useEditor();
+  const { toggleSurroundingMacro } = useEditor();
 
-  function handleSelected(latexMacro: string) {
-    surroundSelectionOrWord(`{${latexMacro}{`, '}}');
+  function handleSelected(macroName: string) {
+    toggleSurroundingMacro(new LatexMacro(macroName, true));
   }
 
   return (
     <NonFocusStealingDropdown name="Font Size" onOptionSelected={handleSelected}>
-      {fontSizes.map(({ latexMacro, friendlyName, fontSize }) => (
-        <NonFocusStealingDropdown.Option value={latexMacro} key={latexMacro}>
+      {fontSizes.map(({ latexMacroName, friendlyName, fontSize }) => (
+        <NonFocusStealingDropdown.Option value={latexMacroName} key={latexMacroName}>
           <Text size={fontSize}>{friendlyName}</Text>
         </NonFocusStealingDropdown.Option>
       ))}
--- src/frontend/src/shared/components/latex-editor/EditorProvider.tsx
+++ src/frontend/src/shared/components/latex-editor/EditorProvider.tsx
@@ -8,11 +8,79 @@ import * as awarenessProtocol from 'y-protocols/awareness';
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
 import { getDocument } from '../../../features/documents/api';
-import { EditorContext, type AwarenessUser, type AwarenessUserList, type EditorContextValue } from './EditorContext';
+import {
+  EditorContext,
+  LatexMacro,
+  type AwarenessUser,
+  type AwarenessUserList,
+  type EditorContextValue,
+} from './EditorContext';
 
 interface EditorProviderProps {
   children: ReactNode;
   roomId: string;
+}
+
+function isImmediatelySurroundedByMacro(
+  macro: LatexMacro,
+  model: MonacoEditor.ITextModel,
+  selection: IRange,
+): [true, IRange] | [false, null] {
+  const macroPrefix = macro.prefix;
+  const macroSuffix = macro.suffix;
+
+  // Determine bounds of surrounding macro
+  const selectionStartOffset = model.getOffsetAt({
+    lineNumber: selection.startLineNumber,
+    column: selection.startColumn,
+  });
+
+  const selectionEndOffset = model.getOffsetAt({
+    lineNumber: selection.endLineNumber,
+    column: selection.endColumn,
+  });
+
+  const prefixStartOffset = selectionStartOffset - macroPrefix.length;
+  const suffixEndOffset = selectionEndOffset + macroSuffix.length;
+
+  // Immediate surroundings are out of bounds of model content => not surrounded
+  if (prefixStartOffset < 0 || suffixEndOffset > model.getValueLength()) {
+    return [false, null];
+  }
+
+  const prefixPosition = model.getPositionAt(prefixStartOffset);
+  const prefixRange: IRange = {
+    startLineNumber: prefixPosition.lineNumber,
+    startColumn: prefixPosition.column,
+    endLineNumber: selection.startLineNumber,
+    endColumn: selection.startColumn,
+  };
+
+  const suffixPosition = model.getPositionAt(suffixEndOffset);
+  const suffixRange: IRange = {
+    startLineNumber: selection.endLineNumber,
+    startColumn: selection.endColumn,
+    endLineNumber: suffixPosition.lineNumber,
+    endColumn: suffixPosition.column,
+  };
+
+  // Check whether surroundings match expected macro
+  const prefixMatches = model.getValueInRange(prefixRange) === macroPrefix;
+  const suffixMatches = model.getValueInRange(suffixRange) === macroSuffix;
+  const isSurrounded = prefixMatches && suffixMatches;
+
+  if (!isSurrounded) {
+    return [false, null];
+  }
+
+  const surroundedMacroRange: IRange = {
+    startLineNumber: prefixPosition.lineNumber,
+    startColumn: prefixPosition.column,
+    endLineNumber: suffixPosition.lineNumber,
+    endColumn: suffixPosition.column,
+  };
+
+  return [true, surroundedMacroRange];
 }
 
 export function EditorProvider({ children, roomId }: EditorProviderProps) {
@@ -38,58 +106,168 @@ export function EditorProvider({ children, roomId }: EditorProviderProps) {
     };
   }, [undoManagerRef]);
 
-  const surroundSelectionOrWord = useCallback(
-    (prefix: string, suffix = '') => {
+  const toggleSurroundingMacro = useCallback(
+    (macro: LatexMacro) => {
       if (!editor || !yDoc || !yText) return;
 
       const model = editor.getModel();
       const selection = editor.getSelection();
       if (!model || !selection) return;
 
-      let range: IRange;
       const position = selection.getPosition();
       const wordAtCursor = model.getWordAtPosition(position);
+      const selectedText = model.getValueInRange(selection);
 
       // Case: User has selected text
       if (!selection.isEmpty()) {
-        range = selection;
+        const [isSurroundedByMacro, surroundedMacroRange] = isImmediatelySurroundedByMacro(macro, model, selection);
+
+        // Replace surrounded macro with selected text
+        if (isSurroundedByMacro) {
+          const surroundingMacroStartOffset = model.getOffsetAt({
+            column: surroundedMacroRange.startColumn,
+            lineNumber: surroundedMacroRange.startLineNumber,
+          });
+
+          const surroundingMacroEndOffset = model.getOffsetAt({
+            column: surroundedMacroRange.endColumn,
+            lineNumber: surroundedMacroRange.endLineNumber,
+          });
+
+          const surroundingMacroLength = surroundingMacroEndOffset - surroundingMacroStartOffset;
+          yDoc.transact(() => {
+            yText.delete(surroundingMacroStartOffset, surroundingMacroLength);
+            yText.insert(surroundingMacroStartOffset, selectedText);
+          }, editorControlRef.current);
+
+          const cursorEndOffset = surroundingMacroStartOffset + selectedText.length;
+          const cursorEndPosition = model.getPositionAt(cursorEndOffset);
+
+          editor.setSelection({
+            startColumn: surroundedMacroRange.startColumn,
+            startLineNumber: surroundedMacroRange.startLineNumber,
+            endColumn: cursorEndPosition.column,
+            endLineNumber: cursorEndPosition.lineNumber,
+          });
+        }
+
+        // Surround selected text with new macro
+        else {
+          const selectionStartPosition = selection.getStartPosition();
+          const selectionStartOffset = model.getOffsetAt(selectionStartPosition);
+          const selectionEndPosition = selection.getEndPosition();
+          const selectionEndOffset = model.getOffsetAt(selectionEndPosition);
+
+          const selectionLength = selectionEndOffset - selectionStartOffset;
+          const surroundedSelection = macro.wrap(selectedText);
+
+          yDoc.transact(() => {
+            yText.delete(selectionStartOffset, selectionLength);
+            yText.insert(selectionStartOffset, surroundedSelection);
+          }, editorControlRef.current);
+
+          const cursorStartOffset = selectionStartOffset + macro.prefix.length;
+          const cursorStartPosition = model.getPositionAt(cursorStartOffset);
+          const cursorEndOffset = selectionStartOffset + surroundedSelection.length - macro.suffix.length;
+          const cursorEndPosition = model.getPositionAt(cursorEndOffset);
+
+          editor.setSelection({
+            startColumn: cursorStartPosition.column,
+            startLineNumber: cursorStartPosition.lineNumber,
+            endColumn: cursorEndPosition.column,
+            endLineNumber: cursorEndPosition.lineNumber,
+          });
+        }
       }
       // Case: User has cursor on top of a word
       else if (wordAtCursor) {
-        range = {
+        const wordRange: IRange = {
           startLineNumber: position.lineNumber,
           startColumn: wordAtCursor.startColumn,
           endLineNumber: position.lineNumber,
           endColumn: wordAtCursor.endColumn,
         };
+
+        const [isSurroundedByMacro, surroundedMacroRange] = isImmediatelySurroundedByMacro(macro, model, wordRange);
+
+        // Replace surrounding macro with word
+        if (isSurroundedByMacro) {
+          const surroundingMacroStartOffset = model.getOffsetAt({
+            column: surroundedMacroRange.startColumn,
+            lineNumber: surroundedMacroRange.startLineNumber,
+          });
+
+          const surroundingMacroEndOffset = model.getOffsetAt({
+            column: surroundedMacroRange.endColumn,
+            lineNumber: surroundedMacroRange.endLineNumber,
+          });
+
+          const surroundingMacroLength = surroundingMacroEndOffset - surroundingMacroStartOffset;
+
+          yDoc.transact(() => {
+            yText.delete(surroundingMacroStartOffset, surroundingMacroLength);
+            yText.insert(surroundingMacroStartOffset, wordAtCursor.word);
+          }, editorControlRef.current);
+
+          const cursorOffset = surroundingMacroStartOffset + wordAtCursor.word.length;
+          const cursorPosition = model.getPositionAt(cursorOffset);
+          editor.setPosition(cursorPosition);
+        }
+
+        // Surround the word with new macro
+        else {
+          const wordStartOffset = model.getOffsetAt({
+            column: wordAtCursor.startColumn,
+            lineNumber: position.lineNumber,
+          });
+
+          const surroundedWord = macro.wrap(wordAtCursor.word);
+
+          yDoc.transact(() => {
+            yText.delete(wordStartOffset, wordAtCursor.word.length);
+            yText.insert(wordStartOffset, surroundedWord);
+          }, editorControlRef.current);
+
+          const cursorOffset = wordStartOffset + surroundedWord.length - macro.suffix.length;
+          const cursorPosition = model.getPositionAt(cursorOffset);
+          editor.setPosition(cursorPosition);
+        }
       }
       // Case: User has cursor in free space
       else {
-        range = selection;
+        const [isSurroundedByMacro, surroundedMacroRange] = isImmediatelySurroundedByMacro(macro, model, selection);
+
+        // Replace surrounded macro with empty text
+        if (isSurroundedByMacro) {
+          const surroundingMacroStartOffset = model.getOffsetAt({
+            column: surroundedMacroRange.startColumn,
+            lineNumber: surroundedMacroRange.startLineNumber,
+          });
+
+          const surroundingMacroEndOffset = model.getOffsetAt({
+            column: surroundedMacroRange.endColumn,
+            lineNumber: surroundedMacroRange.endLineNumber,
+          });
+
+          const surroundingMacroLength = surroundingMacroEndOffset - surroundingMacroStartOffset;
+
+          yDoc.transact(() => {
+            yText.delete(surroundingMacroStartOffset, surroundingMacroLength);
+          }, editorControlRef.current);
+        }
+        // Insert empty macro at cursor position
+        else {
+          const positionOffset = model.getOffsetAt(position);
+          const emptyMacro = macro.empty();
+          yDoc.transact(() => {
+            yText.insert(positionOffset, emptyMacro);
+          }, editorControlRef.current);
+
+          const cursorOffset = positionOffset + emptyMacro.length - macro.suffix.length;
+          const cursorPosition = model.getPositionAt(cursorOffset);
+          editor.setPosition(cursorPosition);
+        }
       }
-
-      const currentText = model.getValueInRange(range);
-      const insertedText = `${prefix}${currentText}${suffix}`;
-
-      // Position where to insert text
-      const startOffset = model.getOffsetAt({
-        lineNumber: range.startLineNumber,
-        column: range.startColumn,
-      });
-      const endOffset = model.getOffsetAt({
-        lineNumber: range.endLineNumber,
-        column: range.endColumn,
-      });
-
-      // Perform insert
-      yDoc.transact(() => {
-        yText.delete(startOffset, endOffset - startOffset);
-        yText.insert(startOffset, insertedText);
-      }, editorControlRef.current);
-
-      // Adjust cursor position
-      const cursorOffset = startOffset + prefix.length + currentText.length + (currentText ? suffix.length : 0);
-      editor.setPosition(model.getPositionAt(cursorOffset));
     },
     [editor, yDoc, yText],
   );
@@ -268,9 +446,9 @@ export function EditorProvider({ children, roomId }: EditorProviderProps) {
       yText,
       undo,
       redo,
-      surroundSelectionOrWord,
+      toggleSurroundingMacro,
     }),
-    [awarenessUsers, currentAwarenessUser, editor, yDoc, yProvider, yText, undo, redo, surroundSelectionOrWord],
+    [awarenessUsers, currentAwarenessUser, editor, yDoc, yProvider, yText, undo, redo, toggleSurroundingMacro],
   );
 
   return <EditorContext.Provider value={value}>{children}</EditorContext.Provider>;
