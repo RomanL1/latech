8d0f562e48cf333d89a8b11ee18e8085dc8db868
RomanL1 <40143010+RomanL1@users.noreply.github.com>
2026-05-25 23:13:38 +0200

    feat(frontend): remember joined documents Closes #95

--- src/frontend/src/pages/document/editor-view/EditorView.tsx
+++ src/frontend/src/pages/document/editor-view/EditorView.tsx
@@ -9,6 +9,7 @@ import type { Document } from '../../../features/documents/document';
 import { getPDFRenderedEventSource, type ResilientEventSource } from '../../../features/pdf-preview/api';
 import { EditorProvider } from '../../../shared/components/latex-editor/EditorProvider';
 import { useGetDocument, useUnlockDocument } from '../../../features/documents/api';
+import { storeDocument } from '../../../features/documents/store';
 import { Button, Flex, Text, TextField } from '@radix-ui/themes';
 
 interface EditorViewProps {
@@ -24,6 +25,15 @@ const EditorView = ({ file, documentId }: EditorViewProps) => {
   const { data: fetchedDocument } = useGetDocument(documentId ?? '');
 
   const unlockMutation = useUnlockDocument(documentId ?? '');
+
+  useEffect(() => {
+    if (!fetchedDocument || !documentId) return;
+    storeDocument({
+      documentId,
+      name: fetchedDocument.name ?? documentId,
+      lastEdited: new Date(),
+    });
+  }, [fetchedDocument, documentId]);
 
   useEffect(() => {
     if (!documentId) return;
